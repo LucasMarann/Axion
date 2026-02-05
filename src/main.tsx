e63e1697690ab2e4d47f130de95f1b97d425e3dd
[dyad] <lucasmaran71@gmail.com>
2026-02-05 11:34:33 -0300

    [dyad] Correção do useNavigate fora do Router - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,11 @@ import "@/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
         <Toaster />
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
